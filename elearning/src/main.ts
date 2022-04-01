0cf82531570c80a675675f57e1c46131174cac3b
j.requioma <j.requioma@arcanys.com>
2022-04-01 11:10:44 +0800

    Add mock service worker

--- elearning/src/main.ts
+++ elearning/src/main.ts
@@ -1,12 +1,16 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import { worker } from './mocks/browser';
+import App from './App.vue';
+import router from './router';
 
 const app = createApp(App);
+
+if (process.env.NODE_ENV === 'development') {
+  worker.start();
+}
 
 app.use(createPinia());
 app.use(router);
 
-app.mount("#app");
+app.mount('#app');
