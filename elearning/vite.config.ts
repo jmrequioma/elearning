a8b9c715abc6fcf49bbe61566189f11ed4afec58
j.requioma <j.requioma@arcanys.com>
2022-04-01 09:44:40 +0800

    Add balm ui

--- elearning/vite.config.ts
+++ elearning/vite.config.ts
@@ -9,6 +9,8 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "balm-ui-plus": "balm-ui/dist/balm-ui-plus.esm.js",
+      "balm-ui-css": "balm-ui/dist/balm-ui.css"
     },
   },
 });
