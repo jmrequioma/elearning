d83bd72d3dfd611bd4e29826a9ef18b21c4a010e
j.requioma <j.requioma@arcanys.com>
2022-04-01 13:43:41 +0800

    Add homepage

--- elearning/src/pages/HomePage.vue
+++ elearning/src/pages/HomePage.vue
@@ -1,13 +1,9 @@
 <template>
-	<div class="app">
+	<div class="home-page">
 		<RouterView />
 	</div>
 </template>
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 </script>
-<style lang="scss" scoped>
-.app {
-	display: flex;
-}
-</style>
+<style></style>
